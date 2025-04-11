5372f3b146606dc0214d9fe16f3cececb0bf344f
Alex Guerra <alexander.a.guerra@gmail.com>
2025-04-11 12:07:31 -0500

    Mass commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
     return (
@@ -19,22 +20,64 @@ export default function Home() {
                 I want to learn to make cool stuff
             </p>
             <p>I am also playing with tailwinds</p>
-
+            <div>
+                <Image
+                    src="/images/gifs/dance.gif"
+                    alt="A picture of me when I do something good."
+                    width={400}
+                    height={400}
+                    className="rounded-full border-4 border-emerald-500"
+                />
+            </div>
             <ul className="flex justify-between text-emerald-700">
-                <li>Juggling</li>
-                <li>Cooking</li>
-                <li>Crafting</li>
+                <li className="text-purple-400">Juggling</li>
+                <li className="text-pink-500">Cooking</li>
+                <li className="text-rose-500">Crafting</li>
                 <li>Drawing</li>
                 <li>Music</li>
             </ul>
-            <h3>It is ok to just write things</h3>
-            <p>No one is going to see this.</p>
+            <h3 className="text-white text-4xl">
+                It is ok to just write things. That is what will help me fill
+                out what this is a going to be
+            </h3>
+            <p>That looks a lot more like a sentence than a title</p>
+            <p>
+                It would be cool to learn more tailwinds. Especially, when it
+                comes to the transformations and stuff. I would also like to
+                understand how the background and gradient system work. I can
+                just keep adding to this page. It doesn't have to really be
+                anything except a hub for navigation. I want to know how to
+                idiomatically style a word in the middle of a p tag. I guess I
+                don't really understand the concept of a span. That might be
+                something for me to ask AI.
+            </p>
+
+            <p>
+                It might be good to make a list of things that I want to ask. Or
+                I could just ask it
+            </p>
+            <h2>Tests for myself</h2>
+            <ul>
+                <li>Make a button that does something</li>
+                <li>Make a card component</li>
+            </ul>
+            <p>
+                No one is going to see this. And that is ok. Just the fact that
+                you have stepped into the ring is what counts. It is just a
+                little iteration of the thing.
+            </p>
+
+            {/* This is almost like the start of a component */}
             <h2>Cool little things to build</h2>
             <ul>
                 <li>A hanafuda card component</li>
                 <li>A task list</li>
+                <li>
+                    Something to help get the kids out the door in the morning.
+                </li>
                 <li>Bird song bingo or memory game</li>
             </ul>
+            {/* This is like the end of the component */}
             <div className="bg-emerald-400">
                 <h2 className="text-5xl">My Favorite Music</h2>
                 <div className="bg-red-200 flex">
