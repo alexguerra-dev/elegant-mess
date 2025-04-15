bc9491b48a848b020fc8d7cb9b05be599a2752d7
Alex Guerra <alexander.a.guerra@gmail.com>
2025-04-15 06:22:54 -0500

    Refactor TaskList and LandingSpot components for improved structure and styling

--- src/components/LandingSpot.tsx
+++ src/components/LandingSpot.tsx
@@ -1,7 +1,12 @@
 export default function LandingSpot() {
     return (
-        <div>
-            <p>Hi</p>
+        <div className="w-1/2 h-1/3 bg-red-500">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <p className="text-2xl">This is a landing spot</p>
+            <p className="text-2xl">Drag a card here</p>
+            <p className="text-2xl">to see the magic happen</p>
+            <p className="text-2xl">or not</p>
+            <p className="text-2xl">or just say hi</p>
         </div>
     )
 }
